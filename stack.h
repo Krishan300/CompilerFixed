6cd5344334ada02e37ff679bd039c7d31451c209
Krishan Madan <krm219@caliban.cse.lehigh.edu>
2016-11-09 01:05:50 -0500

    stackh

---
+++ stack.h
@@ -0,0 +1,50 @@
+/*J Femister
+  CSE 109 - 010
+  Fall 2015
+*/
+
+#ifndef STACK_H
+#define STACK_H
+//#include "hashTable.cc"
+#include <iostream>
+#include "hashTable.h"
+using namespace std;
+//class HashTable;
+class Stack {
+public:
+  // Constructors
+  Stack(int n);
+  Stack(); // Default constructor
+  Stack(const Stack& s); // Copy Constructor - deep copy
+
+  // Destructor
+  ~Stack();
+
+  // Regular Member Functions
+  void push(HashTable val);
+  HashTable pop();
+  HashTable peek() const;
+
+  // Inline Member Function
+  bool isEmpty() const { return tos == EMPTY; } // inline member function
+
+  // Overloaded Operator Friend Functions
+  friend ostream& operator<<(ostream& out, const Stack& s);
+
+  // Overloaded Operator Member Functions
+  const HashTable& operator[](int i) const; // rvalue
+
+  HashTable& operator[](int i); // lvalue  s1[3]  =>  s1.operator[](3);
+
+  int gettosindex();
+private:
+  HashTable *stack;
+  int size;
+  int tos;
+  
+  static const int EMPTY = -1;
+  void checkIndex(int i) const;
+};
+
+#endif
+
