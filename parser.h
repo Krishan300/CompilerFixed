1c51b0e3e7e0ddf7c9e4bcf81c9363a0c3e75a18
Krishan Madan <krm219@caliban.cse.lehigh.edu>
2016-11-09 01:04:23 -0500

    parserh

--- parser.h
+++ parser.h
@@ -3,6 +3,8 @@
 
 #include "Token.h"
 #include "Lexer.h"
+#include "SymbolTable.h"
+#include "hash.h"
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -14,7 +16,7 @@ using namespace std;
 class Parser {
   
 private:
-
+  // SymbolTable tabofsymbols; 
   enum Operation {
     ADD, SUB, MULT, DIV, // Arithmetic Operators
     ISEQ, ISNE, ISLT, ISLE, ISGT, ISGE, // Relational Operators
@@ -22,7 +24,8 @@ private:
     PUSHL, PUSHV, STORE, // Value Transfer Instructions
     JUMP, JUMPF, JUMPT, CALL, RET, // Location Transfer Instructions
     PRINTF, // Misc
-    LABEL, SEQ // Pseudo Operations
+    LABEL, SEQ, // Pseudo Operations
+     PARAM1, PARAM2, PARAM3, PARAM4, PARAM5, FUNC  
   };
   
 public:  
@@ -34,7 +37,7 @@ public:
     string val; // Variable name or jump label
     TreeNode *leftChild;
     TreeNode *rightChild;
-
+    
     // Utility function used by constructors
     void init(Operation opx, string valx, TreeNode *leftChildx, TreeNode *rightChildx) {
        op = opx;
@@ -61,7 +64,7 @@ public:
       init(op, "", leftChild, rightChild);
     }
 
-    static string toString(TreeNode *node) {
+        static string toString(TreeNode *node) {
       return toString0(node, 0);
     }
     
@@ -86,7 +89,7 @@ public:
 	      blanks.substr(0, spaces) + ")";
       }
       return ret;
-    }
+      }
     
   };
   
@@ -96,7 +99,8 @@ private:
   ostream& out;
   int lindex;
   int tindex;
-  
+  //  SymbolTable tabofsymbols;
+  // SymbolTable tabofsymbols;
   string itos(int i) { stringstream ss; ss << i; string res = ss.str(); return res;}
   string makeLabel() { string tmp = "L"; stringstream ss; ss << ++lindex; string res = ss.str(); tmp = tmp + res; return tmp;}
 
@@ -112,24 +116,30 @@ private:
   TreeNode* relationalExpression();
   TreeNode* logicalExpression();
   TreeNode* ifStatement();
-   TreeNode* whileStatement();
-  TreeNode* assignStatement();
+  //   TreeNode* whileStatement();
+  TreeNode* assignStatement(string identname);
   TreeNode* statement();
   TreeNode* block();  
    TreeNode* assignmentExpression();
-  /* TreeNode* returnStatement();
+  TreeNode* funcall(string funcname);
+   TreeNode* vardefStatement();
+  TreeNode* parameterdef(string paramname, int count);
+  TreeNode* returnStatement();
+  TreeNode* Function();
+   TreeNode* compilationUnit();
+  //  TreeNode* returnStatement();
   TreeNode* printfStatement();
   TreeNode* whileStatement();
   
-  TreeNode* assignStatement();
+  /* TreeNode* assignStatement();
   TreeNode* vardefStatement();  
   TreeNode* statement();
   TreeNode* block();
   TreeNode* parameterdef();
-  TreeNode* parameterdefs();
+  TreeNode* parameterdefs(string paramname, int count);
   TreeNode* function();
   TreeNode* compilationunit();*/  
-  
+  //  SymbolTable tabofsymbols;
     
   Parser(Lexer& lexer, ostream& out);
   ~Parser(); 
