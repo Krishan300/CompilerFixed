992182aa9742b167975a7e876e2f95cece9ed212
Krishan Madan <krm219@caliban.cse.lehigh.edu>
2016-11-09 01:01:16 -0500

    ohash

---
+++ hash.h
@@ -0,0 +1,13 @@
+#ifndef HASH_H
+#define HASH_H
+#include <string>
+#include <stdlib.h>
+using namespace std;
+class Hash{
+public:
+  Hash(string);
+  int hash() const;  //return the hash value
+private:
+  int key;  
+};
+#endif
